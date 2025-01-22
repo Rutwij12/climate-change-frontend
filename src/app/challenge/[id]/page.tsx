3232c2835a6c6c5e05cfa4ac26c25b885cd01199
dh722 <dhruv.himatsingka22@imperial.ac.uk>
2025-01-22 11:51:24 +0000

    Fix: Pipeline broken due to unused component

--- src/app/challenge/[id]/page.tsx
+++ src/app/challenge/[id]/page.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { Leaf, CloudRain, Fish, ArrowLeft } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 
