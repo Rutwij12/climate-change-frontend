4713cf04573f76c3f299850b9d3b7b34f419b942
Rutwij Patel <patel.rutwij@gmail.com>
2025-03-06 18:11:36 +0000

    Fix: Linting

--- src/components/GoogleSignInButton.tsx
+++ src/components/GoogleSignInButton.tsx
@@ -15,18 +15,18 @@ const GoogleSignInButton: React.FC = () => {
       try {
         const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login/gauth`, {
           params: {token: credentialResponse.credential}
-          });
+        });
         // Store the information
         const email = response.data.user_email
-            localStorage.setItem('user_email', email);
+        localStorage.setItem('user_email', email);
       } catch (error: unknown) {
-          if (axios.isAxiosError(error)) {
-            // setError(error.response?.data?.detail || "Failed to load authors");
-          } else {
-            // setError("An unexpected error occurred");
-          }
+        if (axios.isAxiosError(error)) {
+          // setError(error.response?.data?.detail || "Failed to load authors");
+        } else {
+          // setError("An unexpected error occurred");
+        }
       } finally {
-            // setLoading(false);
+          // setLoading(false);
       }
         
       router.push('/');
